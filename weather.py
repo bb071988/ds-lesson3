40e1460c36cd0b1ffbe10a4fe959cedd4a6ff44c
bb071988 <bb071988@gmail.com>
2016-04-27 17:59:40 -0500

    final temperature notebook.

---
+++ weather.py
@@ -0,0 +1,41 @@
+# weather api exercise
+
+import datetime
+
+def set_cities():
+	cities = { "Atlanta": '33.762909,-84.422675',
+            "Austin": '30.303936,-97.754355',
+            "Boston": '42.331960,-71.020173',
+            "Chicago": '41.837551,-87.681844',
+            "Cleveland": '41.478462,-81.679435'
+        }
+
+    return(cities)
+
+def get_time():
+	mytime = datetime.datetime
+	api_time = 
+
+
+
+def main():
+	cities = set_cities()
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
+
+#c99c5091c3a50bda392fb7850c54b4ef
+
+# """
+# Create the db
+# for each day in last 30 days
+#     for each city in thelist
+#         get the data
+#             store the city - time and max temp
+
+# """
+
